ec574b4f294c44056a7beeac57f1aebc04a94dfa
lxjxll <898192774@qq.com>
2022-05-19 12:59:28 +0800

    Site updated: 2022-05-19 12:59:26

--- js/custom.js
+++ js/custom.js
@@ -14,36 +14,45 @@ for (var i = 0; i < menus_item_child.length; i++) {
  *
  */
 function loadScript(src, callback) {
-        var script = document.createElement('script'),
-            head = document.getElementsByTagName('head')[0];
-        script.type = 'text/javascript';
-        script.charset = 'UTF-8';
-        script.src = src;
-        if (script.addEventListener) {
-            script.addEventListener('load', function () {
+    var script = document.createElement('script'),
+        head = document.getElementsByTagName('head')[0];
+    script.type = 'text/javascript';
+    script.charset = 'UTF-8';
+    script.src = src;
+    if (script.addEventListener) {
+        script.addEventListener('load', function () {
+            callback();
+        }, false);
+    } else if (script.attachEvent) {
+        script.attachEvent('onreadystatechange', function () {
+            var target = window.event.srcElement;
+            if (target.readyState == 'loaded') {
                 callback();
-            }, false);
-        } else if (script.attachEvent) {
-            script.attachEvent('onreadystatechange', function () {
-                var target = window.event.srcElement;
-                if (target.readyState == 'loaded') {
-                    callback();
-                }
-            });
-        }
-        head.appendChild(script);
+            }
+        });
     }
-    function loadlive2d() {
-        if (document.body.clientWidth > 600) {
-            document.onreadystatechange = function () {
-                if (document.readyState == "complete") {
-                    loadScript('https://cdn.jsdelivr.net/combine/npm/luxiaojie-oss@1.0.4/pixi.min.js,npm/luxiaojie-oss@1.0.4/live2dcubismcore.min.js,npm/luxiaojie-oss@1.0.4/live2dcubismframework.min.js,npm/luxiaojie-oss@1.0.4/live2dcubismpixi.min.js',function(){
-                        loadScript('/js/load.js',function(){
-                            loadModel();
+    head.appendChild(script);
+}
+
+function loadlive2d() {
+    if (document.body.clientWidth > 600) {
+        document.onreadystatechange = function () {
+            if (document.readyState == "complete") {
+                //https://cdn.jsdelivr.net/combine/npm/luxiaojie-oss@1.0.4/pixi.min.js,npm/luxiaojie-oss@1.0.4/live2dcubismcore.min.js,npm/luxiaojie-oss@1.0.4/live2dcubismframework.min.js,npm/luxiaojie-oss@1.0.4/live2dcubismpixi.min.js
+                loadScript('https://npm.elemecdn.com/luxiaojie-oss@latest/pixi.min.js', () => {
+                    loadScript('https://npm.elemecdn.com/luxiaojie-oss@latest/live2dcubismcore.min.js', () => {
+                        loadScript('https://npm.elemecdn.com/luxiaojie-oss@latest/live2dcubismframework.js', () => {
+                            loadScript('https://npm.elemecdn.com/luxiaojie-oss@latest/live2dcubismpixi.js', () => {
+                                loadScript('/js/load.js', function () {
+                                    loadModel();
+                                })
+                            })
                         })
-})
-                }
+                    })
+                })
             }
         }
     }
+}
+
 loadlive2d()
